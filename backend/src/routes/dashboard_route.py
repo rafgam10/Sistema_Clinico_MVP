331a9886d97d03a72624c84aa0f33510d64ec9a0
Timoteo <timoteo@hdinfo-ma.com>
2026-06-16 15:13:39 -0300

    Feat: Ajeitar código

--- backend/src/routes/dashboard_route.py
+++ backend/src/routes/dashboard_route.py
@@ -15,10 +15,10 @@ CACHE_TTL = 300
 @dashboard_bp.route("/pacientes", methods=["GET"])
 def dashboard_paciente_lista():
     try:
-        redis_connection = ConnectionDBRedis()
-        cached = redis_connection.get_cache(CACHE_KEY_PACIENTES)
-        if cached is not None:
-            return jsonify(json.loads(cached)), 200
+        # redis_connection = ConnectionDBRedis()
+        # cached = redis_connection.get_cache(CACHE_KEY_PACIENTES)
+        # if cached is not None:
+        #     return jsonify(json.loads(cached)), 200
         
         with ConnectionDBFireBird() as con:
             cursor = con.cursor()
@@ -37,14 +37,14 @@ def dashboard_paciente_lista():
                     JOIN TBPROFIS TF ON TB.ID_TBPROFIS = TF.ID 
                 WHERE  a.id_tbcencus = '203' AND -- < id_tbcencus > Codigo da unidade
                 tb.cod = '11700' AND -- < TBCBOPRO --:  >
-                CAST(a.DATA_HORA_ENTRADA AS DATE) = CURRENT_DATE - 4;       
+                CAST(a.DATA_HORA_ENTRADA AS DATE) = CURRENT_DATE - 5;       
             """)
 
             columns = [desc[0] for desc in cursor.description]
             rows = cursor.fetchall()
             result = [dict(zip(columns, row)) for row in rows]
             
-            redis_connection.set_cache(CACHE_KEY_PACIENTES, json.dumps(result, default=str), ttl=CACHE_TTL)
+            #redis_connection.set_cache(CACHE_KEY_PACIENTES, json.dumps(result, default=str), ttl=CACHE_TTL)
 
             return jsonify(result), 200
 
