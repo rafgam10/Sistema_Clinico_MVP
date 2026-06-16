2d4103b3d3d8de6b66babbff1a40c7e3a064bac7
João Pedro Cardoso <69486969+JoaoPedro77@users.noreply.github.com>
2026-06-16 15:11:15 -0300

    fix: correção da rota de historico

--- frontend/server/api/historico-paciente/[id].get.ts
+++ frontend/server/api/historico-paciente/[id].get.ts
@@ -1,4 +1,4 @@
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id')
-  return await $fetch(`http://localhost:5000/historico-paciente/${id}`)
+  return await $fetch(`http://localhost:5000/prontuario/historico-paciente/${id}`)
 })
